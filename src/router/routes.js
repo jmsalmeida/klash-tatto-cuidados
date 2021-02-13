db7549cf53578ff7ecaf95b9700d546737ff86ae
James Almeida <James Almeida>
2021-02-13 17:24:16 -0300

    Remove 'Inicio' route and files

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,8 +4,7 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Inicio.vue') },
-      { path: '/limpeza', component: () => import('pages/Limpeza.vue') },
+      { path: '/', component: () => import('pages/Limpeza.vue') },
       { path: '/dicas', component: () => import('pages/Dicas.vue') },
       { path: '/obrigado', component: () => import('pages/Obrigado.vue') },
       { path: '/nao-mexer', component: () => import('pages/NaoMexer.vue') }
